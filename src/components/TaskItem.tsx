83fbdbbf7f3be47779d8d2cd003c571180502ad3
Gloria Morales <gmoraleslondono@gmail.com>
2024-09-04 20:39:34 +0000

    Update TaskItem component to use TypeScript props

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,6 @@
-export const TaskItem = ({ text, id, onClick }) => (
+type TaskItemProps = {text: string, id: number, onClick: (id: number) => void};
+
+export const TaskItem = ({ text, id, onClick }: TaskItemProps ) => (
     <div className="task-item-container">
       <p className="task-title">{text}</p>
       <button className="delete-task-button" onClick={() => onClick(id)}>
