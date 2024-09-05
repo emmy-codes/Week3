fb4d7479a2e739ddd6d1b60ed2a4035e6ba7867f
Emma <alunia.moon@gmail.com>
2024-09-05 16:03:31 +0200

    first try changing types on redux/jsx

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, MouseEvent } from 'react';
 import { useDispatch } from "react-redux";
-import { addTask } from "../redux/tasks"
+import { addTask } from "../redux/tasks";
 
 export const AddTask = () => {
-    const [value, setValue] = useState('')
+    const [value, setValue] = useState<string>('')
 
     const dispatch = useDispatch();
 
-    const onSubmit = (event) => {
+    type addTaskTypes = {
+        task: string;
+        completed: boolean;
+    }
+
+
+    const onSubmit = (event: MouseEvent) => {
         event.preventDefault();
 
-        if (value.trim().length === 0) {
+        if (value === null) {
             alert("Enter a task before adding !!");
             setValue("");
             return;
@@ -35,7 +41,8 @@ export const AddTask = () => {
                 placeholder="Add a task"
                 value={value}
                 onChange={(event) => setValue(event.target.value)}
-            ></input>
+            >
+            </input>
 
             <button className="add-task-button" onClick={onSubmit}>
                 Save
