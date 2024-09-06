e2d44ecf1dd01cdbe0960f80e268d59d8bc8b985
Emma <alunia.moon@gmail.com>
2024-09-06 21:32:58 +0200

    update main.tsx to expect HTMLElements
    typeGuard is used to ensure that there is an element with the ID of root, and if there is it'll continue to the render. An extra precaution from TS it seems :)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const typeGuard = (maybeHTMLElement: HTMLElement | null): maybeHTMLElement is HTMLElement => {
+return maybeHTMLElement != null
+}
+
+const root = document.getElementById('root');
+
+if (!typeGuard(root)) throw new Error('No root element found');
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
