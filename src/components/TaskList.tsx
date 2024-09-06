a72f6226a30b5664b516726d92d358d1309808e3
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-09-06 06:59:20 +0200

    Removed redundant spaces

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { TaskItem } from "../components/TaskItem";
 import { deleteTask } from "../redux/tasks";
-import {Task} from "../redux/tasks";
-
+import { Task } from "../redux/tasks";
 
 export const TaskList = () => {
     // Use Redux's useSelector hook to get the current state of the tasks array.
@@ -11,22 +10,18 @@ export const TaskList = () => {
     const dispatch = useDispatch();
 
     const handleClick = (id: number) => {
-        dispatch(
-           deleteTask({
-                id,
-            })
-        );
+        dispatch(deleteTask({ id }));
     };
 
     return (
         <ul className="task-list">
             {tasks.map((task: Task) => (
                 <TaskItem
-                 key={task.id} 
-                 text={task.text} 
-                 id={task.id} 
-                 onClick={handleClick}
-                 />
+                    key={task.id}
+                    text={task.text}
+                    id={task.id}
+                    onClick={handleClick}
+                />
             ))}
         </ul>
     );
